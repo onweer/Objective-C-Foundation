3c6f39230eee6152b67fe4d6b6aab61997a85280
Zongminglee <zongmingli@outlook.com>
2016-01-19 17:37:15 +0800

    add 浅复制，深复制

---
+++ 浅复制与深复制/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  浅复制与深复制
+//
+//  Created by SpiColorPendra on 16/1/19.
+//  Copyright © 2016年 SpiColorPendra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // 如果复制对象的时候存在多级引用。那么就有可能复制的时候 后一级的引用还是指向同一个对象
+    }
+    return 0;
+}
