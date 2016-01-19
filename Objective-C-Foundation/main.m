a786933ffb455ee55bf496b5914702ba7a66fe59
Zongminglee <zongmingli@outlook.com>
2016-01-19 10:13:42 +0800

    add foundation project

---
+++ Objective-C-Foundation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C-Foundation
+//
+//  Created by SpiColorPendra on 16/1/19.
+//  Copyright © 2016年 SpiColorPendra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
