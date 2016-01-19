6d047a7c86800bb0c996e4a905be0a6b822955c4
Zongminglee <zongmingli@outlook.com>
2016-01-19 17:58:23 +0800

    集合框架概述! 又是集合 ArrayList呢？HashMap<K,V>?

---
+++ Objc-集合概述/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  Objc-集合概述
+//
+//  Created by SpiColorPendra on 16/1/19.
+//  Copyright © 2016年 SpiColorPendra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // OC的集合类是一种特别有用的工具类，它可以用于存储数量不等的多个对象，并可以实现常用的数据结构，如栈，队列
+        // 等，除此之外，OC集合还可以用于保存具有映射关系的关联数组，OC集合大致可以分为：NSArray、NSSet和NSDictionary
+        // 三种体系,NSArray代表有序，可重复，NSSet代表无序，不可重复，NSDictionary则代表具有映射关系的集合.
+        // OC集合就像一种容器，可以把对象"丢进"容器中
+        
+        //在编程的时候常常需要面对集中存放多个数据，
+        // 初始化数组的时候  数学:80  语文:79 这种虽然看上去是两个数组，但是两个数组之间有一定的关联关系。
+        // 为了保存数量不确定的数据，以及保存具有映射关系的数据，OC提供了集合类
+        // 集合类也被称为容器类
+    }
+    return 0;
+}
