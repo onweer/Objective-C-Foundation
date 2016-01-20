2684383f6f9a289168ba70aab2d6a45cf449fdaf
Zongminglee <zongmingli@outlook.com>
2016-01-20 15:15:58 +0800

    add NSMutableSet explaination

---
+++ NSMutableSet/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  NSMutableSet
+//
+//  Created by SpiColorPendra on 16/1/20.
+//  Copyright © 2016年 SpiColorPendra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // NSMutableSet 仅仅是代表了NSSet的可变集合
+        // 添加了如下方法:
+        //  1.addObject:
+        //  2.removeObject:
+        //  3. removeAllObjects:
+        //  4.addObjectsFromArray:
+        //  5.unionSet:  计算并集
+        //  6.minusSet:  计算差集
+        //  7.intersectSet: 计算交集
+        //  8.setSet  用后一个替换已有集合中所有的元素
+        
+    }
+    return 0;
+}
